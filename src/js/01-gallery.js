9854dab3679ea8a306e84f5e80dd0575b934d89c
SnehaShisNasty <katirina18@gmail.com>
2023-11-03 21:02:38 +0000

    done first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const list = document.querySelector('.gallery');
+list.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
 
-console.log(galleryItems);
+function createMarkup(arr) {
+  return arr
+    .map(
+      item => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}"  data-title="123" ">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="large-image.jpg"
+            alt="${item.description}"
+            />
+        </a>
+    </li>
+  `
+    )
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery .gallery__link', {
+  captionDelay: 250,
+  captionType: 'attr',
+  captionsData: 'alt',
+});
+console.log(SimpleLightbox);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+player.on('play', function () {
+  player.getCurrentTime().then(function (seconds) {
+    console.log('Current time:', seconds);
+  });
+});
